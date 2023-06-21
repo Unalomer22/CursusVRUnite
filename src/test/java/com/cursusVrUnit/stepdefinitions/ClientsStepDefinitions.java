4590b44c01db26befacacee3d1781201a5598c5c
Ebubekir SAHIN <sahinebubekir@yahoo.com>
2023-06-21 23:25:50 +0200

    Client tabinda 2 test case eklendi

--- src/test/java/com/cursusVrUnit/stepdefinitions/ClientsStepDefinitions.java
+++ src/test/java/com/cursusVrUnit/stepdefinitions/ClientsStepDefinitions.java
@@ -3,21 +3,37 @@ package com.cursusVrUnit.stepdefinitions;
 import com.cursusVrUnit.pages.ClientsPage;
 import com.cursusVrUnit.utilities.BrowserUtils;
 import com.cursusVrUnit.utilities.Driver;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en_old.Ac;
 import org.checkerframework.checker.units.qual.C;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClientsStepDefinitions {
     ClientsPage clientsPage = new ClientsPage();
-
+    static Faker faker = new Faker();
+    List<String> expectedClientDatas = new ArrayList<>();
+    Actions actions = new Actions(Driver.getDriver());
     int expectedSayfa = 1;
     int actualSayfaPrevious;
+
+    String clientName;
+    String clientBSNNumber;
+    String clientAddress;
+    String clientPostCode;
+    String clientCountry;
+    String clientPersonalName;
+    String clientPhoneNumber;
+    String clientEmail;
+    String clientExpireDate;
 
     @And("Clients tabindaki arama kutusuna {string} musterisinin bilgisi yazilir")
     public void clientsTabindakiAramaKutusunaMusterisininIsmiYazilir(String arananMusteriBilgisi) {
@@ -87,7 +103,7 @@ public class ClientsStepDefinitions {
     @Then("Sayfanin bir mevcut sayfadan bir sonraki sayfaya gectigi gozlemlenir")
     public void sayfaninBirOncekiSayfadanBirSonrakiSayfayaGectigiGozlemlenir() {
         String actualUrl = Driver.getDriver().getCurrentUrl();
-        int actualSayfa = Integer.parseInt(actualUrl.substring(actualUrl.length()-1));
+        int actualSayfa = Integer.parseInt(actualUrl.substring(actualUrl.length() - 1));
         Assert.assertEquals(expectedSayfa + 1, actualSayfa);
     }
 
@@ -100,10 +116,77 @@ public class ClientsStepDefinitions {
         Assert.assertEquals(actualSayfaPrevious - 1, actualSayfa);
     }
 
+    @Then("Sayfanin {string}.ci sayfada oldugu dogrulanir")
+    public void sayfaninCiSayfadaOlduguDogrulanir(String expectedPageNumber) {
+        String actualPageNumber = Driver.getDriver().getCurrentUrl();
+        actualPageNumber = actualPageNumber.charAt(actualPageNumber.length() - 1) + "";
+        Assert.assertEquals(expectedPageNumber, actualPageNumber);
+    }
+
+    @And("Create New Client butonuna tiklanir")
+    public void createNewClientButonunaTiklanir() {
+        clientsPage.createNewClientButton.click();
+    }
+
+    @And("Gerekli valid Client bilgileri girilir")
+    public void gerekliValidClientBilgileriGirilir() {
+        clientName = faker.name().fullName();
+        clientBSNNumber = faker.number().digits(23);
+        clientAddress = faker.address().city();
+        clientPostCode = faker.address().zipCode();
+        clientCountry = faker.address().country();
+        clientPersonalName = faker.name().fullName();
+        clientPhoneNumber = faker.phoneNumber().cellPhone();
+        clientEmail = faker.internet().emailAddress();
+        clientExpireDate = "2023-08-05";
+
+        expectedClientDatas.add(clientName);
+        expectedClientDatas.add(clientBSNNumber);
+        expectedClientDatas.add(clientPhoneNumber);
+        expectedClientDatas.add(clientCountry);
+        expectedClientDatas.add(clientExpireDate);
+
+
+        actions.
+                sendKeys(clientsPage.clientNameTextBox, clientName).
+                sendKeys(Keys.TAB).
+                sendKeys(clientBSNNumber).
+                sendKeys(Keys.TAB).
+                sendKeys(clientAddress).
+                sendKeys(Keys.TAB).
+                sendKeys(clientPostCode).
+                sendKeys(Keys.TAB).
+                sendKeys(clientCountry).
+                sendKeys(Keys.TAB).
+                sendKeys(clientPersonalName).
+                sendKeys(Keys.TAB)
+                .sendKeys(clientPhoneNumber).
+                sendKeys(Keys.TAB).
+                sendKeys(clientEmail).
+                sendKeys(Keys.TAB).
+                sendKeys(clientExpireDate).
+                perform();
+        BrowserUtils.waitFor(3);
+        Driver.getDriver().findElement(By.xpath("//html")).click();
+        BrowserUtils.waitFor(3);
+    }
+
     public static void main(String[] args) {
-        String actualUrl = "http://peteksoft.com:9000/clients?page=2";
-        System.out.println(actualUrl.lastIndexOf(actualUrl.length() - 1));
-        System.out.println();
+        Faker faker = new Faker();
+        System.out.println(faker.phoneNumber().cellPhone());
+        System.out.println(faker.phoneNumber().phoneNumber());
+    }
+
+    @And("Client sayfasindaki Save Changes butonuna tiklanir")
+    public void clientSayfasindakiSaveChangesButonunaTiklanir() {
+        clientsPage.saveChangesButton.click();
+    }
+
+    @Then("Olusturulan clientin verileri listeden dogrulanir")
+    public void olusturulanClientinVerileriListedenDogrulanir() {
+        for (int i = 0; i < 5; i++){
+            Assert.assertEquals(expectedClientDatas.get(i),clientsPage.firstClientsDatas.get(i).getText());
+        }
     }
 }
 
